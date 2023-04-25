b497706c812a86c12ae971786dcb3d7f179e8509
sayyss <sawaisayam@gmail.com>
2023-04-25 15:31:09 -0500

    added testing for Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,37 @@
+import java.util.Scanner;
+
+
 public class Main {
 
 
     public static void main(String[] args) {
 
+
+        Scanner GameScanner = new Scanner(System.in);
+
+        System.out.println("Enter difficulty level for the game(Easy(0), Medium(1), Hard(2): ");
+        String diffLevel = GameScanner.nextLine();
+
+        Minefield minefield; 
+        if(diffLevel.equals("0")){
+          minefield = new Minefield(5,5,5);
+        }
+        if(diffLevel.equals("1")){
+          minefield = new Minefield(9,9,12);
+        }
+        else{
+          minefield = new Minefield(20,20,40);
+        }
+
+        
+        minefield.createMines(2,2,5);
+        minefield.evaluateField();
+
+        minefield.revealMines(3,5);
+
+        minefield.printMinefield();
+
+        /*
         Minefield minefield = new Minefield(5, 5, 5);
         minefield.createMines(2, 2, 5);
         // Print initial state of minefield
@@ -22,7 +51,7 @@ public class Main {
         // Evaluate field after guessing mine at (2,2)
         System.out.println("Evaluating field after guessing mine at (2,2)");
         minefield.evaluateField();
-        minefield.printMinefield();
+        minefield.printMinefield();*/
     }
 
 }
